b0440b16349c02e1c50c39eb5e7eaa4fbeddab44
Andreas Streichardt <mop@karthago.globalpark.de>
2011-01-14 18:58:47 +0100

    Version 0.2.1

--- php_lua.h
+++ php_lua.h
@@ -24,7 +24,7 @@
 extern zend_module_entry lua_module_entry;
 #define phpext_lua_ptr &lua_module_entry
 
-#define PHP_LUA_VERSION "0.2.1-dev"
+#define PHP_LUA_VERSION "0.2.1"
 
 #ifdef PHP_WIN32
 #define PHP_LUA_API __declspec(dllexport)
